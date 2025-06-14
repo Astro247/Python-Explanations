0b81bf38170a88f6f85617d44527dd0bc02bedf8
Astro247 <manuelsarti40@gmail.com>
2025-06-14 20:04:17 +0200

    First Pathlib Explanation: Differences between 'Path' and 'PurePath' explained.

---
+++ Pathlib/pathlib_path-purepath.py
@@ -0,0 +1,10 @@
+"""
+La libreria pathlib consente di gestire e manipolare percorsi di file e cartelle in modo multipiattaforma, 
+evitando i problemi legati alle differenze sintattiche tra i sistemi operativi come Windows e Linux.
+"""
+
+from pathlib import Path, PurePath
+
+path = Path("disco/directory/file") # La classe 'Path' permette l'accesso ad una path locale e alla modifica del filesystem (quindi di file/cartelle presenti in quel path).
+
+path2 = PurePath("disco/directory/file") # La classe 'PurePath', invece, si limita ad interagire/manipolare solo ed esclusivamente con la stringa path, non potendo lavorare con il filesystem (cioè con i contenuti di quella path).
