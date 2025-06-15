800f0f4eb0d7854e2b17c3932ce7dc439d32e6cc
Astro247 <manuelsarti40@gmail.com>
2025-06-15 18:24:18 +0200

    Update Pathlib Explanation: Differences between 'Path' and 'PurePath' explained.

--- Pathlib/pathlib_path-purepath.py
+++ Pathlib/pathlib_path-purepath.py
@@ -5,11 +5,21 @@ evitando i problemi legati alle differenze sintattiche tra i sistemi operativi c
 
 from pathlib import Path, PurePath
 
-path = Path("disco/directory/file") # La classe 'Path' permette l'accesso ad una path locale e alla modifica del filesystem (quindi di file/cartelle presenti in quel path).
+path = Path("disco/directory/file") # La classe 'Path' contiene metodi che modificano il filesystem (quindi di file/cartelle presenti in quel path).
 
-path2 = PurePath("disco/directory/file") # La classe 'PurePath', invece, si limita ad interagire/manipolare solo ed esclusivamente con la stringa path, non potendo lavorare con il filesystem (cioè con i contenuti di quella path).
+path2 = PurePath("disco/directory/file") # La classe 'PurePath', invece, contiene metodi che si limitano ad interagire/manipolare solo ed esclusivamente con la stringa path, non potendo lavorare con il filesystem (cioè con i contenuti di quella path).
 
 """
 'PosixPath' è il nome attribuito alla sintassi con cui vengono scritte le stringhe path in Linux, mentre per windows
 sono chiamate 'WindowsPath'
+
+Sia la classe "Path" che la classe "PurePath", in quanto classi, non sono "path" effettive, ma creano oggetti con una serie di
+chiavi-valori che rappresentano quella path, per esempio:
+
+se -- p = Path("file.txt")
+
+p.exists → Ritorna un bool che dice se il file esiste o meno.
+p.name → Ritorna il nome del file (punto finale di una path assoluta)
+p.parent → Ritorna il nome della directory parent del file (In questo caso "Pathlib")
+p.suffix → Restituisce il formato del file, in questo caso '.txt'
 """
